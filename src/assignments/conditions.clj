32839ddcea141bae852df520dfd98d26e9f5b956
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2020-02-04 10:02:41 +0530

    Implement conditions-apply

--- src/assignments/conditions.clj
+++ src/assignments/conditions.clj
@@ -54,6 +54,12 @@
           (> x y) :greece
           :else :universe))
 
+
+(defn is-single-occurance [subcoll supercoll]
+  (not (or
+         (empty? (filter (into #{} subcoll) supercoll))
+         (not= subcoll (filter (into #{} subcoll) supercoll)))))
+
 (defn conditions-apply
   "Given a collection of any length, returns:
   :wonder-woman if collection has a single occurrence of 1 and 3 in that order
@@ -63,8 +69,13 @@
   {:level        :medium
    :use          '[condp filter]
    :alternates   '[if cond]
-   :implemented? false}
-  [coll])
+   :implemented? true}
+  [coll] is-single-occurance coll
+  [1 3] :wonder-woman
+  [:a ::c] :durga
+  [[2 3] [4 5]] :cleopatra
+  :tuntun
+  )
 
 (defn repeat-and-truncate
   "Given coll and options to repeat and truncate
@@ -73,10 +84,11 @@
   (repeat-and-truncate (range 4) true true 6) => '(0 1 2 3 0 1)"
   {:level        :medium
    :use          '[cond->> concat take]
-   :implemented? false}
+   :implemented? true}
   [coll rep? truncate? n] (cond->> coll
-                                   rep? (concat (flatten (repeat coll)))
-                                   truncate? (take n)))
+                                   rep? (cycle)
+                                   truncate? (drop-last n)
+                                   :else (take n)))
 
 (defn order-in-words
   "Given x, y and z, returns a vector consisting of
@@ -123,6 +135,6 @@
    :use          '[as-> reverse]
    :implemented? true}
   [coll] (as-> coll l1
-               (map inc l1)
+               (if (every? (partial instance? Number) l1) (map inc l1) l1)
                (cons 0 l1)
-               (concat (reverse l1) (map inc coll))))
+               (concat (reverse l1) (if (every? (partial instance? Number) coll) (map inc coll) coll))))
